2016abb27c87059a7e1688375c7eeddd777635e0
Rodrigo Teixeira <88559255+RodrigoMSCTeixeira@users.noreply.github.com>
2022-12-01 18:35:18 -0300

    Alteração

--- ts/index.ts
+++ ts/index.ts
@@ -6,6 +6,15 @@ const totalHours = hours + 6
 const totalMinutes = (totalHours - hours) * 60
 const totalSeconds = totalMinutes * 60
 
+const beforeStartCount = () => {
+    const inputDate:any = document.getElementById('date')
+    const inputStart:any = document.getElementById('start')
+    const inputExit:any = document.getElementById('exit')
+    inputDate.value = isToday.toLocaleDateString()
+    inputStart.value = `${hours}:${minutes}:${seconds}`
+    inputExit.value = `${totalHours}:${minutes}:${seconds}`
+}
+ 
 const refresh = (timer: number) => {
     const seconds: any = document.getElementById('seconds')
     const minutes: any = document.getElementById('minutes')
@@ -22,12 +31,24 @@ const refresh = (timer: number) => {
 }
 
 const countdown = (timer: number) => {
+    let sec = 0
+    const intervalStart = 9900
+    const intervalEnd = 10800
    const count = () => {
         if(timer === 0) {
             stopCountdown(id)
         } 
         refresh(timer)
         timer--
+        sec ++
+        if(sec === intervalStart) {
+            const inputIntervalStart: any = document.getElementById('intervalStart')
+            inputIntervalStart.value = `${jobInterval().hours}:${jobInterval().minutes}:${jobInterval().seconds}`
+        }
+        if(sec === intervalEnd) {
+            const inputIntervalEnd: any = document.getElementById('intervalEnd')
+            inputIntervalEnd.value = `${jobInterval().hours}:${jobInterval().minutes}:${jobInterval().seconds}`
+        }
     }
     const id = setInterval(count, 1000) 
 }
@@ -47,13 +68,8 @@ const stopCountdown = (id: number) => {
     // cell3.innerText = `${finalHour}:${finalMinute}`
     row.append(cell1, cell2, cell3)
     table.append(row)
-
-    const inputDate:any = document.getElementById('date')
-    const inputStart:any = document.getElementById('start')
     const inputEnd:any = document.getElementById('end')
     // const inputExtraHour:any = document.getElementById('extraHour')
-    inputDate.value = isToday.toLocaleDateString()
-    inputStart.value = `${hours}:${minutes}:${seconds}`
     inputEnd.value = `${finalHour}:${finalMinute}:${finalSeconds}`
     autoClick()
 }
@@ -63,5 +79,18 @@ const autoClick = () => {
     buttonSend.dispatchEvent(new MouseEvent("click"));
 };
 
-countdown(7200)
+const jobInterval = () => {
+    const date = new Date()
+    const hours = date.getHours()
+    const minutes = date.getMinutes()
+    const seconds = date.getSeconds()
+    const jobInterval =  {
+        hours: hours,
+        minutes: minutes,
+        seconds: seconds
+    }
+    return jobInterval
+}
 
+countdown(totalSeconds)
+beforeStartCount()
