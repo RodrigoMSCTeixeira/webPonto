e04fec7f7cc440ea2e67d3f236c42f2d7b3ed6dc
Rodrigo Teixeira <88559255+RodrigoMSCTeixeira@users.noreply.github.com>
2022-12-02 20:16:26 -0300

    Alteração no typescript e adição do form

--- ts/index.ts
+++ ts/index.ts
@@ -1,23 +1,14 @@
-let hours: any = new Date().getHours()
-let minutes: any = new Date().getMinutes()
-let seconds: any = new Date().getSeconds() 
+const date  = new Date()
+let hours: any = date.getHours()
+let minutes: any = date.getMinutes()
+let seconds: any = date.getSeconds()
 const isToday = new Date(Date.now())
-let totalHours: any = hours + 6
-let intervalStartHour = hours + 3
-let intervalStartMinutes: any =  minutes + 45
-let intervalEndMinutes = intervalStartMinutes + 15
-let totalMinutes: any = (totalHours - hours) * 60
-// const totalSeconds = totalMinutes * 60
-let countSeconds = 0
 
 const clock = () => {
-    const date = new Date()
+    const date  = new Date()
     let hh: any = date.getHours()
     let mn: any = date.getMinutes()
     let sc: any = date.getSeconds()
-    const intervalStart = 9900
-    const intervalEnd = 10800
-    const totalSeconds = 21600
     hh = hh < 10 ? `0${hh}` : hh
     mn = mn < 10 ? `0${mn}` : mn
     sc = sc < 10 ? `0${sc}` : sc
@@ -27,71 +18,109 @@ const clock = () => {
     seconds.innerText = sc
     minutes.innerText = mn
     hours.innerText = hh
-    countSeconds ++
-
-    if(countSeconds == intervalStart) {
-        const inputIntervalStart: any = document.getElementById('intervalStart')
-        inputIntervalStart.value = `${hh}:${mn}:${sc}`
-    }
-
-    if(countSeconds == intervalEnd) {
-        const inputIntervalEnd: any = document.getElementById('intervalEnd')
-        inputIntervalEnd.value = `${hh}:${mn}:${sc}`
-    }
-
-    if(countSeconds == totalSeconds) {
-      clearInterval(id)
-      stopCountdown()
-    }
 }
 
 const beforeStartCount = () => {
-    const inputDate:any = document.getElementById('date')
-    const inputStart:any = document.getElementById('start')
-    const inputIntervalStart: any = document.getElementById('intervalStart')
-    const inputIntervalEnd: any = document.getElementById('intervalEnd')
-    const inputExit:any = document.getElementById('exit')
-    hours = hours < 10 ? `0${hours}` : hours
-    minutes = minutes < 10 ? `0${minutes}` : minutes
-    seconds = seconds < 10 ? `0${seconds}` : seconds
-    totalHours = totalHours > 23 ?  `0${totalHours - 24}` : `${totalHours}`
-    intervalStartMinutes = intervalStartMinutes > 59 ? `${intervalStartMinutes - 60}` : intervalStartMinutes
-    intervalEndMinutes  = intervalEndMinutes > 59 ? `${intervalEndMinutes - 60}` : intervalEndMinutes
-    intervalStartMinutes = intervalStartMinutes < 10 ? `0${intervalStartMinutes}` : intervalStartMinutes
-    intervalEndMinutes = intervalEndMinutes < 10 ? `0${intervalEndMinutes}` : intervalEndMinutes
-    inputDate.value = isToday.toLocaleDateString()
-    inputStart.value = `${hours}:${minutes}:${seconds}`
-    inputIntervalStart.value = `${intervalStartHour}:${intervalStartMinutes}:${seconds}`
-    inputIntervalEnd.value = `${intervalStartHour}:${intervalEndMinutes}:${seconds}`
-    inputExit.value = `${totalHours}:${minutes}:${seconds}`
+    if(hours == 7 && minutes >= 30) {
+        let totalHours: any = hours + 6
+        let intervalStartHour = 0
+        let intervalStartTime: any = 0
+        if(minutes < 15) {
+            intervalStartHour = hours + 2
+            intervalStartTime =  (60 - 15) + minutes
+        } else {
+            intervalStartHour = hours + 3
+            intervalStartTime =  minutes - 15
+        } 
+    
+        let intervalEndTime: any =  intervalStartTime + 15
+    
+        if (intervalEndTime > 59) {
+            intervalEndTime = intervalEndTime - 60
+        } else {
+            intervalEndTime = intervalEndTime
+        }
+        const inputDate:any = document.getElementById('date')
+        const inputStart:any = document.getElementById('input')
+        const inputIntervalStart: any = document.getElementById('intervalStart')
+        const inputIntervalEnd: any = document.getElementById('intervalEnd')
+        const inputExit:any = document.getElementById('exit')
+        hours = hours < 10 ? `0${hours}` : hours
+        minutes = minutes < 10 ? `0${minutes}` : minutes
+        seconds = seconds < 10 ? `0${seconds}` : seconds
+        totalHours = totalHours > 23 ?  `0${totalHours - 24}` : `${totalHours}`
+        intervalStartTime = intervalStartTime < 10 ? `0${intervalStartTime}` : intervalStartTime
+        let intervalEndHour = minutes < 15 ? `${intervalStartHour + 1}` : intervalStartHour
+        intervalEndTime = intervalEndTime < 10 ? `0${intervalEndTime}` : intervalEndTime
+        inputDate.value = isToday.toLocaleDateString()
+        inputStart.value = `${hours}:${minutes}:${seconds}`
+        inputIntervalStart.value = `${intervalStartHour}:${intervalStartTime}:${seconds}`
+        inputIntervalEnd.value = `${intervalEndHour}:${intervalEndTime}:${seconds}`
+        inputExit.value = `${totalHours}:${minutes}:${seconds}`
+    }
+   
+
 }
  
 const stopCountdown = () => {
-    const table: any = document.getElementById('calendar-body')
-    const row = document.createElement('tr')
-    const finalHour = new Date().getHours()
-    const finalMinute = new Date().getMinutes()
-    const finalSeconds = new Date().getSeconds()
-    const cell1 = document.createElement('td')
-    const cell2 = document.createElement('td')
-    const cell3 = document.createElement('td')
-    // cell1.innerText = isToday.toLocaleDateString()
-    // cell2.innerText = `${hours}:${minutes}`
-    // cell3.innerText = `${finalHour}:${finalMinute}`
-    row.append(cell1, cell2, cell3)
-    table.append(row)
-    const inputEnd:any = document.getElementById('end')
-    // const inputExtraHour:any = document.getElementById('extraHour')
-    inputEnd.value = `${finalHour}:${finalMinute}:${finalSeconds}`
-    autoClick()
+    const inputStart: any = document.getElementById('input')
+
+    console.log(inputStart.value)
+    if(inputStart.value != '') {
+        clearInterval(id)
+        let finalHour: any = new Date().getHours()
+        let finalMinute: any = new Date().getMinutes()
+        let finalSeconds: any = new Date().getSeconds()
+        finalHour = finalHour < 10 ? `0${finalHour}` : finalHour
+        finalMinute = finalMinute < 10 ? `0${finalMinute}` : finalMinute
+        finalSeconds = finalSeconds < 10 ? `0${finalSeconds}` : finalSeconds
+        const inputEnd:any = document.getElementById('output')
+        const inputExtraHourStart: any = document.getElementById('extraHourStart')
+        const inputExtraHourEnd: any = document.getElementById('extraHourEnd')
+        inputEnd.value = `${finalHour}:${finalMinute}:${finalSeconds}`
+        if(inputExtraHourStart.value != '') {
+            inputExtraHourEnd.value = `${finalHour}:${finalMinute}:${finalSeconds}`
+        }
+        autoClick()
+    }
 }
 
 const autoClick = () => {
     const buttonSend:any = document.getElementById("submit");
+    buttonSend.setAttribute('onclick','exportData()')
     buttonSend.dispatchEvent(new MouseEvent("click"));
 };
 
-beforeStartCount()
+
+const extraHour = () => {
+    let hh: any = document.getElementById('hours')?.innerText
+    let mn: any = document.getElementById('minutes')?.innerText
+    let sc: any = document.getElementById('seconds')?.innerText
+    const end = '1330'
+
+    if(end != `${hh}${mn}`) {
+        const inputExtraHourStart: any = document.getElementById('extraHourStart')
+        inputExtraHourStart.value = `${hh}:${mn}:${sc}`
+    }
+
+}
+
+const optionStart: any = document.getElementById('start')
+const optionExtraHour: any = document.getElementById('extraHour')
+const optionFinish: any = document.getElementById('end')
+
+optionStart.addEventListener('click', () => {
+    beforeStartCount()
+})
+
+optionExtraHour.addEventListener('click', () => {
+    extraHour()
+})
+
+optionFinish.addEventListener('click', () => {
+    stopCountdown()
+})
+
 const id = setInterval(clock, 1000)
 
 
